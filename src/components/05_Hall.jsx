e25cb3ef18e86b419cd44905dba3cf84b677ab17
jetwat <jetwatwarathip@gmail.com>
2026-04-22 15:24:18 +0700

    Correct Corridor variable names

--- src/components/05_Hall.jsx
+++ src/components/05_Hall.jsx
@@ -1,12 +1,12 @@
-import Coridor from "./06_Coridor";
+import Corridor from "./06_Corridor";
 
 export default function Hall({question, answer, handleAnswer}) {
     
     return (
         <div className="flex flex-col justify-center items-center pt-10 bg-green-400 w-[90%]">
             <h1>Hall</h1>
-            {/* Coridor */}
-            <Coridor question={question} answer={answer} handleAnswer={handleAnswer}/>
+            {/* Corridor */}
+            <Corridor question={question} answer={answer} handleAnswer={handleAnswer}/>
         </div>
     );
 }
